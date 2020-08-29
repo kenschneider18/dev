f3d73fb9e6490b366d0a052aa99c521dc35e3cc3
Ken Schneider <ken.schneider18@gmail.com>
2020-08-29 00:59:51 -0400

    Refactor work to executor

---
+++ pkg/executor/executor.go
@@ -0,0 +1,242 @@
+package executor
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	// GET downloads a repo creating a directory structure
+	// based on the repo URL
+	GET = iota
+	// INSTALL runs GET then takes the extra step of running
+	// `make devbin` and copies the resulting binary to $DEVPATH/bin
+	// If $DEVPATH/bin is part of your $PATH, these binaries will be
+	// immediately accesible via the command line
+	INSTALL
+	// INITIALIZE creates an empty directory at the specified repo URL
+	// within $DEVPATH. The programming language of choice can be passed
+	// for supported languages to do additional setup.
+	INITIALIZE
+)
+
+var (
+	// ValidCommands maps a command line argument to a command
+	ValidCommands = map[string]Command{
+		"get":     GET,
+		"install": INSTALL,
+		"init":    INITIALIZE,
+	}
+)
+
+type (
+	// Command represents the action to be executed
+	Command int
+
+	fn func([]string) error
+
+	// Executor runs a command
+	Executor struct {
+		devbinDir   string
+		binDir      string
+		prevDir     string
+		devPath     string
+		command     Command
+		functionMap map[Command]func([]string) error
+	}
+)
+
+// New returns a new executor to run a command with
+func New(devbinDir, binDir, prevDir, devPath string, command Command) *Executor {
+	return &Executor{
+		devbinDir: devbinDir,
+		binDir:    binDir,
+		prevDir:   prevDir,
+		devPath:   devPath,
+		command:   command,
+	}
+}
+
+func (e *Executor) Execute(args []string) error {
+	// this should probably turn into a map at some point
+	switch e.command {
+	case GET:
+		if len(args) != 1 {
+			return errors.Errorf("get expects 1 argument, found %d", len(args))
+		}
+		return e.clone(args)
+	case INSTALL:
+		if len(args) != 1 {
+			return errors.Errorf("install expects 1 argument, found %d", len(args))
+		}
+		err := e.clone(args)
+		if err != nil {
+			return err
+		}
+		return e.install()
+	case INITIALIZE:
+		return e.init(args)
+	default:
+		return errors.Errorf("command not found")
+	}
+}
+
+func (e *Executor) clone(args []string) error {
+	if len(args) != 1 {
+		return errors.Errorf("expects 1 argument, found %d", len(args))
+	}
+
+	// TODO: allow this to be done with or without a prefix
+	// default clones via HTTPS but supports either protocol
+	path := args[0]
+
+	// TODO: add -u flag to prevent this dialogue and update the existing
+	// clone ALSO allow passthrough so if someone has run get and then
+	// runs install later it will still do the install portion of the code but
+	// won't re-clone unless -u is passed
+	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		return errors.New("Repo already exists in DEVPATH... Stopping.")
+	}
+
+	if strings.HasPrefix(path, "https://") || strings.HasPrefix(path, "git@") {
+		return errors.New("invalid git repo prefix: do not include protocol prefixes such as https:// or git@")
+	}
+
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		cleanUp(path)
+		return err
+	}
+
+	err = os.Chdir(path)
+	if e, ok := err.(*os.PathError); ok && err != nil {
+		return errors.Wrapf(e.Unwrap(), "failed to open directory %q", path)
+	} else if err != nil {
+		return errors.Wrapf(err, "failed to open directory %q", path)
+	}
+
+	command := "git"
+	commandArgs := []string{"clone", fmt.Sprintf("https://%s", path), "."}
+
+	cmd := exec.Command(command, commandArgs...)
+	stdErrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		cleanUp(path)
+		return errors.Wrap(err, "failed to read from git")
+	}
+
+	if err = cmd.Start(); err != nil {
+		cleanUp(path)
+		return errors.Wrap(err, "failed to clone git repo")
+	}
+
+	stdErr, _ := ioutil.ReadAll(stdErrPipe)
+
+	if err = cmd.Wait(); err != nil {
+		cleanUp(path)
+		return errors.Wrapf(err, "failed to clone repo: %s", string(stdErr))
+	}
+
+	// TODO: only print output of git clone if
+	// -v flag passed
+	log.Print(string(stdErr))
+
+	return nil
+}
+
+func (e *Executor) install() error {
+	// Run make
+	cmd := exec.Command("make", "devbin")
+	stdErrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to read output from make")
+	}
+
+	stdOutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to read output from make")
+	}
+
+	if err = cmd.Start(); err != nil {
+		return errors.Wrap(err, "failed running make devbin")
+	}
+
+	stdErr, _ := ioutil.ReadAll(stdErrPipe)
+	stdOut, _ := ioutil.ReadAll(stdOutPipe)
+
+	if err = cmd.Wait(); err != nil {
+		return errors.Wrapf(err, "failed to run make devbin %s", string(stdErr))
+	}
+
+	// TODO: only print output of make
+	// -v flag passed
+	log.Printf(string(stdOut))
+
+	// open devbin directory
+	err = os.Chdir(e.devbinDir)
+	if pathErr, ok := err.(*os.PathError); ok && err != nil {
+		return errors.Wrapf(pathErr.Unwrap(), "failed to open %s", e.devbinDir)
+	} else if err != nil {
+		return errors.Wrapf(err, "failed to open %s", e.devbinDir)
+	}
+
+	filesToMove := make([]string, 0, 5)
+	err = filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return errors.Wrap(err, "failed to ls devbin")
+		}
+
+		if info.IsDir() || path == "." || path == ".." {
+			return nil
+		}
+
+		filesToMove = append(filesToMove, path)
+
+		return nil
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to walk directory")
+	}
+
+	log.Println("Installing binaries:", filesToMove)
+
+	for _, file := range filesToMove {
+		err = os.Rename(file, fmt.Sprintf("%s/%s/%s", e.devPath, e.binDir, file))
+		if err != nil {
+			log.Fatalf("Failed to move file: %s", file)
+		}
+	}
+
+	// open parent directory
+	err = os.Chdir(e.prevDir)
+	if e, ok := err.(*os.PathError); ok && err != nil {
+		log.Fatalf("Failed to open parent directory: %s", e.Unwrap().Error())
+	} else if err != nil {
+		log.Fatalf("Failed to open parent directory: %s", err.Error())
+	}
+
+	err = os.RemoveAll(e.devbinDir)
+	if err != nil {
+		log.Fatalf("Failed to remove directory %q: %s", e.devbinDir, err.Error())
+	}
+	return nil
+}
+
+func (e *Executor) init(args []string) error {
+	return nil
+}
+
+func cleanUp(path string) error {
+	err := os.RemoveAll(path)
+	if err != nil {
+		return errors.Wrap(err, "failed to clean up created directory structure")
+	}
+	return nil
+}
